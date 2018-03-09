fa72f8ba4933cacb3e50f5c35afe992d09e98c80
Alex Kocharin <alex@kocharin.ru>
2018-03-09 13:16:48 +0000

    Add profile widget for clubs

---
+++ server/users/member/blocks/clubs.js
@@ -0,0 +1,39 @@
+// Fill a list of clubs this user has joined
+//
+'use strict';
+
+
+const _             = require('lodash');
+const sanitize_club = require('nodeca.clubs/lib/sanitizers/club');
+
+
+module.exports = function (N) {
+
+  // Fetch user blog entries
+  //
+  N.wire.after('server:users.member', async function fetch_user_blog_entries(env) {
+    let membership = await N.models.clubs.ClubMember.find()
+                               .where('user').equals(env.data.user._id)
+                               .lean(true);
+
+    let clubs;
+
+    if (membership.length > 0) {
+      clubs = await N.models.clubs.Club.find()
+                        .where('_id').in(_.map(membership, 'club'))
+                        .lean(true);
+    }
+
+    clubs = _.sortBy(clubs, 'title');
+    clubs = await sanitize_club(N, clubs, env.user_info);
+
+    if (env.user_info.user_hid !== env.data.user.hid && clubs.length === 0) return;
+
+    env.res.blocks = env.res.blocks || {};
+
+    _.set(env.res, 'blocks.clubs', {
+      list:  clubs,
+      count: clubs.length
+    });
+  });
+};
