6c21066b5dfc3f8ebf215198f28c4a1a8ff1502e
Alex Kocharin <alex@kocharin.ru>
2018-05-01 16:57:07 +0000

    Implement setting club avatar

--- client/clubs/sole/edit/edit.js
+++ client/clubs/sole/edit/edit.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const identicon    = require('nodeca.users/lib/identicon');
+const avatarWidth  = '$$ JSON.stringify(N.config.users.avatars.resize.orig.width) $$';
+const avatarHeight = '$$ JSON.stringify(N.config.users.avatars.resize.orig.width) $$';
+
 let pageParams;
 
 
@@ -15,6 +19,65 @@ N.wire.on(module.apiPath + ':save', function save_club(data) {
 
   if (data.$this[0].checkValidity() === false) return;
 
-  return N.io.rpc('clubs.sole.edit.update', data.fields)
+  let fields = Object.assign({}, data.fields);
+
+  if (data.$this[0].avatar.files.length > 0) {
+    fields.avatar = data.$this[0].avatar.files[0];
+  }
+
+  fields.remove_avatar = Boolean(fields.remove_avatar);
+
+  return N.io.rpc('clubs.sole.edit.update', fields)
     .then(() => N.wire.emit('navigate.to', { apiPath: 'clubs.sole', params: pageParams }));
+});
+
+
+N.wire.on(module.apiPath + ':avatar_change', function avatar_change(data) {
+  let reader = new FileReader();
+
+  reader.onload = function (e) {
+    let img = $('.club-avatar__image');
+
+    img.attr('src', e.target.result);
+
+    // adjust image offset and size so user will only see the center part,
+    // which image will be cropped into on the server
+    img.one('load', () => {
+      $('.club-avatar').addClass('club-avatar__m-exists');
+      $('.club-avatar').closest('form')[0].remove_avatar.value = false;
+
+      img.css('width', 'auto');
+      img.css('height', 'auto');
+
+      let width = img.width();
+      let height = img.height();
+
+      let toWidth = avatarWidth;
+      let toHeight = avatarHeight;
+
+      let scaleX, scaleY;
+
+      if (width / height > toWidth / toHeight) {
+        scaleX = width * toHeight / height;
+        scaleY = toHeight;
+      } else {
+        scaleX = toWidth;
+        scaleY = height * toWidth / width;
+      }
+
+      img.css('width', Math.floor(scaleX));
+      img.css('height', Math.floor(scaleY));
+      img.css('left', Math.floor((scaleX - toWidth) / -2));
+      img.css('top', Math.floor((scaleY - toHeight) / -2));
+    });
+  };
+
+  reader.readAsDataURL(data.$this[0].files[0]);
+});
+
+
+N.wire.on(module.apiPath + ':avatar_remove', function avatar_remove() {
+  $('.club-avatar').removeClass('club-avatar__m-exists');
+  $('.club-avatar').closest('form')[0].remove_avatar.value = 'yes';
+  $('.club-avatar__image').attr('src', identicon($('.club-avatar').closest('form')[0].club_id.value, avatarWidth));
 });
--- server/clubs/sole/edit/update.js
+++ server/clubs/sole/edit/update.js
@@ -4,15 +4,22 @@
 'use strict';
 
 
-const charcount = require('charcount');
+const charcount   = require('charcount');
+const crypto      = require('crypto');
+const fs          = require('mz/fs');
+const sharp       = require('sharp');
+const resizeParse = require('nodeca.users/server/_lib/resize_parse');
+const resize      = require('nodeca.users/models/users/_lib/resize');
 
 
 module.exports = function (N, apiPath) {
 
   N.validate(apiPath, {
-    club_id:     { format: 'mongo', required: true },
-    title:       { type: 'string',  required: true },
-    description: { type: 'string',  required: true }
+    club_id:       { format: 'mongo', required: true },
+    title:         { type: 'string',  required: true },
+    description:   { type: 'string',  required: true },
+    remove_avatar: { type: 'boolean', required: true },
+    avatar:        { type: 'string' }
   });
 
 
@@ -65,14 +72,97 @@ module.exports = function (N, apiPath) {
   });
 
 
+  // Save new avatar if it's uploaded
+  //
+  N.wire.before(apiPath, async function save_avatar(env) {
+    let fileInfo = env.req.files.avatar && env.req.files.avatar[0];
+    if (!fileInfo) return;
+
+    let config = resizeParse(N.config.users.avatars);
+    let ext = (fileInfo.headers['content-type'] || '').split('/').pop();
+    let typeConfig = config.types[ext];
+
+    if (!typeConfig) {
+      throw {
+        code: N.io.CLIENT_ERROR,
+        message: env.t('err_unsupported_image')
+      };
+    }
+
+    let tmpfile = '/tmp/club-avatar-' + crypto.pseudoRandomBytes(8).toString('hex') + '.jpg';
+
+    let sharpInstance = sharp(fileInfo.path);
+
+    sharpInstance.rotate()
+                 .resize(config.resize.orig.width, config.resize.orig.height)
+                 .crop(sharp.strategy.center);
+
+    try {
+      await sharpInstance.toFile(tmpfile);
+    } catch (__) {
+      throw {
+        code: N.io.CLIENT_ERROR,
+        message: env.t('err_bad_image')
+      };
+    }
+
+    try {
+      let data = await resize(tmpfile, {
+        store: N.models.core.File,
+        ext,
+        maxSize: typeConfig.max_size,
+        resize: typeConfig.resize
+      });
+
+      env.data.remove_old_avatar = env.data.club.avatar_id;
+      env.data.new_avatar = data.id;
+    } finally {
+      await fs.unlink(tmpfile);
+    }
+  });
+
+
+  // Set old avatar for removing if flag is set
+  //
+  N.wire.before(apiPath, function remove_avatar(env) {
+    if (env.params.remove_avatar) {
+      env.data.remove_old_avatar = env.data.club.avatar_id;
+    }
+  });
+
+
   // Update club info
   //
   N.wire.on(apiPath, function update_club(env) {
-    return N.models.clubs.Club.update({ _id: env.data.club._id }, {
+    let update_data = {
       $set: {
         title: env.params.title,
         description: env.params.description
       }
-    });
+    };
+
+    if (env.data.new_avatar) {
+      update_data.$set.avatar_id = env.data.new_avatar;
+    } else if (env.data.remove_old_avatar) {
+      update_data.$unset = { avatar_id: true };
+    }
+
+    return N.models.clubs.Club.update({ _id: env.data.club._id }, update_data);
+  });
+
+
+  // Remove old avatar
+  //
+  N.wire.after(apiPath, async function remove_old_avatar(env) {
+    if (!env.data.remove_old_avatar) return;
+
+    await N.models.core.File.remove(env.data.remove_old_avatar, true);
+  });
+
+
+  // Mark user as active
+  //
+  N.wire.after(apiPath, async function set_active_flag(env) {
+    await N.wire.emit('internal:users.mark_user_active', env);
   });
 };
