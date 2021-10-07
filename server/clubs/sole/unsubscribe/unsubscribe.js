a47a21a1b7cd21f3f5f364d26070d17bc5455df7
Alex Kocharin <alex@kocharin.ru>
2021-10-07 20:33:41 +0300

    Remove MUTED options from some subscription dialogs

--- server/clubs/sole/unsubscribe/unsubscribe.js
+++ server/clubs/sole/unsubscribe/unsubscribe.js
@@ -1,6 +1,6 @@
 // Update subscription type and show unsubscribe club page
 //
-// `WATCHING|TRACKING -> NORMAL -> MUTED`
+// `WATCHING|TRACKING -> NORMAL`
 //
 'use strict';
 
@@ -68,9 +68,6 @@ module.exports = function (N, apiPath) {
     if ([ Subscription.types.WATCHING, Subscription.types.TRACKING ].indexOf(curType) !== -1) {
       // `WATCHING|TRACKING -> NORMAL`
       updatedType = Subscription.types.NORMAL;
-    } else if (curType === Subscription.types.NORMAL) {
-      // `NORMAL -> MUTED`
-      updatedType = Subscription.types.MUTED;
     } else {
       // Nothing to update here, just fill subscription type
       env.res.subscription = curType;
