16f15e662b42041dcfdbded636db8de8cf0c5331
Alex Kocharin <alex@kocharin.ru>
2021-12-31 18:52:46 +0300

    Prevent duplicate notifications

--- server/clubs/topic/post/reply/reply.js
+++ server/clubs/topic/post/reply/reply.js
@@ -319,13 +319,14 @@ module.exports = function (N, apiPath) {
         });
 
         // avoid sending both reply and new_comment notification to the same user
-        ignore.push(String(env.data.new_post.to));
+        ignore.push(String(env.data.new_post.to_user));
       }
     }
 
     await N.wire.emit('internal:users.notify', {
       src: env.data.new_post._id,
-      type: 'CLUBS_NEW_POST'
+      type: 'CLUBS_NEW_POST',
+      ignore
     });
   });
 
